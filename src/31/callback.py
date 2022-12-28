6878ebd2afc82b52a91e77c6103d604efe245ddd
Jose Luis Chiquete <josech@gmail.com>
2022-12-27 19:55:36 -0600

    221227b

--- src/31/callback.py
+++ src/31/callback.py
@@ -23,4 +23,4 @@ def update_output_div(input_value):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8080, host="0.0.0.0")
+    app.run_server(debug=True, port=8000, host="0.0.0.0")
